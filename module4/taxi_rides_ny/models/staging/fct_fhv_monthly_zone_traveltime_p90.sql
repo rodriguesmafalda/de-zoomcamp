c3ea655ec3884b42f20c9a1f9ed47642af0c9de2
Mafalda Rodrigues <rodriguess.mafalda@gmail.com>
2025-02-26 22:57:19 +0000

    test

--- module4/taxi_rides_ny/models/staging/fct_fhv_monthly_zone_traveltime_p90.sql
+++ module4/taxi_rides_ny/models/staging/fct_fhv_monthly_zone_traveltime_p90.sql
@@ -4,9 +4,6 @@ WITH trip_dur_perc AS (
         dropoff_zone,
         year,
         month,
-        PERCENTILE_CONT(
-            TIMESTAMP_DIFF(pickup_datetime, dropoff_datetime, SECOND),
-            0.90
-        ) OVER (PARTITION BY year, month, pickup_zone, dropoff_zone) AS p90
+        PERCENTILE_CONT(TIMESTAMP_DIFF(pickup_datetime, dropoff_datetime, SECOND), 0.90) OVER (PARTITION BY year, month, pickup_locationid, dropoff_locationid) AS p90
     FROM {{ ref('dim_fhv_trips') }}
 )
